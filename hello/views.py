d8d158e52b16e40500f80ad173cac0465b23baaf
szc <suzichao@gmail.com>
2023-05-13 10:03:33 +0800

    dj4e 13 fix 1st

--- hello/views.py
+++ hello/views.py
@@ -4,6 +4,6 @@ def myview(request):
     num_vists = request.session.get('num_visits', 0) + 1
     request.session['num_visits'] = num_vists
     if num_vists > 4 : del(request.session['num_visits'])
-    resp = HttpResponse('view counts = ' + str(num_vists))
+    resp = HttpResponse('view count = ' + str(num_vists))
     resp.set_cookie('dj4e_cookie', '7314392b', max_age=1000)
     return resp
